023724c868145422ae2e978c26f078e7bc208f5d
Gabriel <gabrielborgeslopes12@gamil.com>
2024-12-06 19:57:31 -0300

    exercicioEBACjs

---
+++ formulario.js
@@ -0,0 +1,27 @@
+const forms = document.getElementById("forms")
+const n1 = document.getElementById("n1")
+const n2 = document.getElementById("n2")
+
+function ValidaNumber(num1, num2){
+
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    return num2 > num1
+}
+
+forms.addEventListener("submit", function(e){
+    e.preventDefault()
+
+    const num1 = parseFloat(n1.value)
+    const num2 = parsefloat(n2.value)
+
+    const result = VaidaNumber(num1 ,num2)
+
+    if(result){
+        alert("Parabéns, o campo 2 é maior que o campo 1")
+    }else{
+        alert("o campo 2 não é maoir")
+    }
+})
+
